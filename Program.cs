31d19c25a1d931c124fe10ac7a148e58de4d48db
davinazka <irsanteluma@gmail.com>
2026-02-09 10:54:20 +0700

    Menambahkan sistem memilih aksi dan pemenang/akhir pertandingan

--- Program.cs
+++ Program.cs
@@ -5,14 +5,56 @@ Console.WriteLine("Permainan Dimulai... \n");
 wizardA.ShowStats();
 wizardB.ShowStats();
 
-wizardA.Attack(wizardB); //itu yang di dalam parameter itu targetnya
-wizardB.Attack(wizardA);
-wizardA.Attack(wizardB);
-wizardA.Heal(wizardA);
+//wizardA.Attack(wizardB); //itu yang di dalam parameter itu targetnya
+//wizardB.Attack(wizardA);
+//wizardA.Attack(wizardB);
+//wizardA.Heal(wizardA);
+string pilihan ="";
+
+while (wizardA.Energy > 0 && wizardB.Energy > 0)
+{
+    Console.WriteLine("Pilih salah satu aksi berikut : ");
+    Console.WriteLine($"1. {wizardA.Name} menyerang {wizardB.Name}");
+    Console.WriteLine($"2. {wizardB.Name} menyerang {wizardA.Name}");
+    Console.WriteLine($"3. {wizardA.Name} melakukan heal");
+    Console.WriteLine($"4. {wizardB.Name} melakukan heal");
+
+    Console.Write("\nMasukkan pilihanmu (1/2/3/4) : ");
+    pilihan = Console.ReadLine();
+    if (pilihan == "1")
+    {
+        wizardA.Attack(wizardB);
+    }
+    else if (pilihan == "2")
+    {
+        wizardB.Attack(wizardA);
+    }
+    else if (pilihan == "3")
+    {
+        wizardA.Heal(wizardA);
+    }
+    else if (pilihan == "4")
+    {
+        wizardB.Heal(wizardB);
+    }
+    else
+    {
+        Console.WriteLine("Pilihan tidak valid!!");
+    }
+}
 
 Console.WriteLine("Permainan Berakhir... \n");
 wizardA.ShowStats();
 wizardB.ShowStats();
+
+if (wizardA.Energy > wizardB.Energy)
+{
+    Console.WriteLine($"{wizardA.Name} memenangkan duel");
+}
+else
+{
+    Console.WriteLine($"{wizardB.Name} memenangkan duel");
+}
 
 public class Wizard
 {
